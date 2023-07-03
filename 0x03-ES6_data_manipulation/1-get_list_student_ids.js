61dfede8b5b5671fef91a1795357bfaae7823ccf
neneeye <neneeye@gmail.com>
2023-07-03 20:18:02 +0000

    data mani

---
+++ 0x03-ES6_data_manipulation/1-get_list_student_ids.js
@@ -0,0 +1,13 @@
+/**
+ * Returns an array of student IDs from a list of student objects
+ *
+ * @param {Array} lst - the list of student objects
+ * @returns {Array} an array of student IDs or an empty array if lst is not an array
+ */
+export default function getListStudentIds(lst) {
+  if (Array.isArray(lst)) {
+    return lst.map((obj) => obj.id);
+  }
+
+  return [];
+}
