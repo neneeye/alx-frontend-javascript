3767430960b23da845b8963f835bec3c7beaa4e0
neneeye <neneeye@gmail.com>
2023-07-03 20:41:29 +0000

    data manipulat

--- 0x03-ES6_data_manipulation/1-get_list_student_ids.js
+++ 0x03-ES6_data_manipulation/1-get_list_student_ids.js
@@ -1,5 +1,5 @@
 /**
- * Returns an array of student IDs from a list of student objects
+ * Returns an array of student IDs from a list of the student objects
  *
  * @param {Array} lst - the list of student objects
  * @returns {Array} an array of student IDs or an empty array if lst is not an array
